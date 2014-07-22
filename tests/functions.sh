41475b2154983afde913fe3aa843a6a4eb555dd5
Dmitry Lavnikevich <haff@midgard.by>
2014-07-22 23:28:12 +0300

    tests: Little stylistic changes.

--- tests/functions.sh
+++ tests/functions.sh
@@ -6,7 +6,7 @@
 # Copy testing root into trut directory
 # @ROOT_PATH --- full path to testing root directory
 trut_copy_root () {
-    ROOT_PATH="${1}"
+    ROOT_PATH="$1"
     echo -n "Copying testing root from '${ROOT_PATH}'..."
     if [ -d "${ROOT_PATH}" ]; then
         cd "${ROOT_PATH}"
@@ -34,9 +34,9 @@ trut_init () {
 # Check files existance
 # @FILE_NAME --- files path relative to trut direcotry
 trut_check_file () {
-    local FILE_NAME="${1}"
+    local FILE_NAME="$1"
     echo -n "Checking if file '${FILE_NAME}' exists..."
-    if [ -d "${FILE_NAME}" ]; then
+    if [ -f "${FILE_NAME}" ]; then
         echo "OK"
     else
         echo "FAIL"
@@ -47,7 +47,7 @@ trut_check_file () {
 # Check directory existance
 # @DIR_NAME --- files path relative to trut direcotry
 trut_check_dir () {
-    local DIR_NAME="${1}"
+    local DIR_NAME="$1"
     echo -n "Checking if directory '${DIR_NAME}' exists..."
     if [ -d "${DIR_NAME}" ]; then
         echo "OK"
